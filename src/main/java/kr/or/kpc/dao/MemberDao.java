85b10d28743b39af3d3813a4f0607c9b9be20e7f
youngwoo2020 <youngwoo2020@naver.com>
2021-06-08 17:52:21 +0900

    2021-06-08

--- src/main/java/kr/or/kpc/dao/MemberDao.java
+++ src/main/java/kr/or/kpc/dao/MemberDao.java
@@ -6,13 +6,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-
 import kr.or.kpc.dto.MemberDto;
 import kr.or.kpc.util.ConnLocator;
 
-
-
-public class MemberDao  {
+public class MemberDao {
 	private static MemberDao single;
 
 	private MemberDao() {
@@ -24,50 +21,44 @@ public class MemberDao  {
 		}
 		return single;
 	}
-	
-	
-	public ArrayList<MemberDto> select(int start, int len) {
+	public ArrayList<MemberDto> select(int start , int len) {
 		ArrayList<MemberDto> list = new ArrayList<MemberDto>();
-
-	
-
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-
+		
 		try {
-			con = ConnLocator.getConnection();
+			con = ConnLocator.getConnect();
+			
 			StringBuilder sql = new StringBuilder();
 			sql.append("SELECT num, NAME, addr ");
 			sql.append("FROM member ");
-			sql.append("ORDER BY num DESC ");
+			sql.append("ORDER BY num desc ");
 			sql.append("LIMIT ?,? ");
-
 			pstmt = con.prepareStatement(sql.toString());
+			
 			int index = 1;
-			pstmt.setInt(index++, start); // 매개변수
+			pstmt.setInt(index++, start);
 			pstmt.setInt(index, len);
-
+			
 			rs = pstmt.executeQuery();
-
-			while (rs.next()) {
+			
+			while(rs.next()) {
 				index = 1;
-				int num = rs.getInt(index++);// rs.getInt("num");
-				String name = rs.getString(index++);// rs.getInt("name");
-				String addr = rs.getString(index);// rs.getInt("addr");
-				list.add(new MemberDto());
+				int num = rs.getInt(index++);// or rs.getInt("num");
+				String name = rs.getString(index++);//rs.getString("name");
+				String addr = rs.getString(index);//rs.getString("addr");
+				list.add(new MemberDto(num, name, addr));
 			}
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
+			
 			try {
-				if (con != null)
-					con.close();  //connection 자원을 반납 
-				if (pstmt != null)
-					pstmt.close();
-				if (rs != null)
-					rs.close();
+				if(con != null) con.close();//Connection 자원을 반납
+				if(pstmt != null) pstmt.close();
+				if(rs != null) rs.close();
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -80,5 +71,6 @@ public class MemberDao  {
 	
 	
 	
-
+	
+	
 }
--- src/main/java/kr/or/kpc/util/ConnLocator.java
+++ src/main/java/kr/or/kpc/util/ConnLocator.java
@@ -9,22 +9,22 @@ import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class ConnLocator {
-	public static Connection getConnection() {
+	public static Connection getConnect() {
+		//DataSource을 이용해서 Connection 객체를 생성하면
+		//2가지 이점이 있음
+		//1. 드라이브 로드와 DriverManager 역할을 한다.
+		//2. ConnectionPool를 사용한다.
 		DataSource ds = null;
 		Connection con = null;
 		
-		
 		try {
 			Context context = new InitialContext();
-			//java:comp/env/는 고정
-			//jdbc.kpc => context.xml의 name 속성을 참조해야 함. 
-			
-			ds = (DataSource) context.lookup("java:comp/env/jdbc/kpc");
+			//java:comp/env/ 는 고정
+			//jdbc/kpc => context.xml의 name 속성 참조해야 함.
+			ds = (DataSource) context.lookup(
+					"java:comp/env/jdbc/kpc");
 			//ConnectionPool에 만들어진 Connection 객체를 반환
 			con = ds.getConnection();
-		
-	
-		
 		} catch (NamingException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -33,7 +33,5 @@ public class ConnLocator {
 			e.printStackTrace();
 		}
 		return con;
-		
 	}
-
 }
